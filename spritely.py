56e8b268b4749efa7ffd70ef88dad23be6b8fbbc
DarBab <119262402+DarBab@users.noreply.github.com>
2025-03-30 12:30:01 -0400

    Add files via upload

--- spritely.py
+++ spritely.py
@@ -10,8 +10,9 @@ monster = sheet.subsurface(17,32,16,31)
 monster = p.transform.scale2x(monster)
 bert = sheet.subsurface(0,0,16,16)
 bert = p.transform.scale2x(bert)
-cube =[0] * 20
-b = [0] * 20
+
+cube = [0] * 200
+b = [0] * 200
 cube[2]= sheet.subsurface(0,160,32,32)
 cube[2]= p.transform.scale2x(cube[2])
 cube[1]= sheet.subsurface(0,192,32,32)
@@ -22,7 +23,8 @@ bert_Group = p.sprite.Group()
 block_Group = p.sprite.Group()
 monster_Group = p.sprite.Group()
 
-class qbert(p.sprite.Sprite):
+
+class char(p.sprite.Sprite):
     def __init__(self,image):
         super().__init__()
         self.image = image
@@ -36,49 +38,38 @@ class block(p.sprite.Sprite):
         self.rect = self.image.get_rect() 
         self.rect.center = (x,y)   
 
-me = qbert(bert)
+me = char(monster)
 bert_Group.add(me)
 x = 500
 y = 200
-b[0] = block(cube[0],x,y)
-
-b[1] = block(cube[0],x-32,200+48)
-b[2] = block(cube[0],x+32,200+48)
-
-b[3] = block(cube[0],x-64,200+96)
-b[4] = block(cube[0],x,200+96)
-b[5] = block(cube[0],x+64,200+96)
-
-b[6] = block(cube[0],x-96,200+144)
-b[7] = block(cube[0],x-32,200+144)
-b[8] = block(cube[0],x+32,200+144)
-b[9] = block(cube[0],x+96,200+144)
-
-b[10] = block(cube[0],x-128,200+192)
-b[11] = block(cube[0],x-64,200+192)
-b[12] = block(cube[0],x,200+192)
-b[13] = block(cube[0],x+64,200+192)
-b[14] = block(cube[0],x+128,200+192)
-for i in range(15):
-    block_Group.add(b[i])
-
+dx = 32
+dy = 48
+n = 0
+for i in range(10):
+  y+=48
+  for j in range(i+1):
+    b[n] = block(cube[0],x,y)
+    block_Group.add(b[n])
+    n+=1
+print(n)
+   
 p.display.set_caption("Bab bert")
 run = True
 while run:
-        for event  in p.event.get():
-            if event.type == p.QUIT:
-                run = False
-        if p.sprite.collide_mask(b[0],me):
-            b[9].image = cube[2]
-        screen.fill("BLUE")
-        mx,my = p.mouse.get_pos()
-        print(mx,my)
-        me.rect.center = (500,170)
-        block_Group.clear(screen,screen)
-        block_Group.draw(screen)
-        bert_Group.draw(screen)
-        block_Group.update()
-        bert_Group.update()
-        p.display.update()
+  for event  in p.event.get():
+    if event.type == p.QUIT:
+      run = False
+  if p.sprite.collide_mask(b[0],me):
+    b[0].image = cube[2]
+  screen.fill("BLUE")
+  mx,my = p.mouse.get_pos()
+  print(mx,my)
+  me.rect.center = (mx,my)
+  block_Group.clear(screen,screen)
+  block_Group.draw(screen)
+  bert_Group.draw(screen)
+  block_Group.update()
+  bert_Group.update()
+  p.display.update()
 
 
