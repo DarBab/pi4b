e97b5f2224840fe9f628e81bdf33d27213b2411e
DarBab <119262402+DarBab@users.noreply.github.com>
2025-04-06 23:10:22 -0400

    Add files via upload

--- spritely.py
+++ spritely.py
@@ -1,59 +1,69 @@
 import pygame as p
-import time as t
 p.init()
-tick = p.time.Clock()
+clock = p.time.Clock()
 width = 1024
 height = 768
 screen = p.display.set_mode((width,height))
 sheet = p.image.load("/home/pi/Downloads/qbert_sheet.png").convert_alpha()
-monster = sheet.subsurface(17,32,16,31)
-monster = p.transform.scale2x(monster)
-bert = sheet.subsurface(0,0,16,16)
-bert = p.transform.scale2x(bert)
-
 cube = [0] * 200
-b = [0] * 200
-cube[2]= sheet.subsurface(0,160,32,32)
-cube[2]= p.transform.scale2x(cube[2])
-cube[1]= sheet.subsurface(0,192,32,32)
-cube[1]= p.transform.scale2x(cube[1])
-cube[0]= sheet.subsurface(0,224,32,32)
-cube[0]= p.transform.scale2x(cube[0])
+b = [0] * 60
+monsters = [0] * 10
+bert = [0] * 10
+
+for i in range(8):
+  monsters[i] = p.transform.scale2x(sheet.subsurface(i*16,32,16,32))
+  bert[i] = p.transform.scale2x(sheet.subsurface(i*16,0,16,16))
+
+for cl in range(3):
+  tc = 160+(cl*32)   
+  cube[cl]= sheet.subsurface(0,tc,32,32)
+  cube[cl]= p.transform.scale2x(cube[cl])
+
 bert_Group = p.sprite.Group()
 block_Group = p.sprite.Group()
 monster_Group = p.sprite.Group()
 
-
-class char(p.sprite.Sprite):
+class qbert(p.sprite.Sprite):
     def __init__(self,image):
         super().__init__()
         self.image = image
         self.rect = self.image.get_rect()
-        self.rect.center = (0,0)
+        self.rect.center = (500,170)
+    def rot(self):
+#        print(c,tc)
+        me.image = bert[1]
 
 class block(p.sprite.Sprite):
     def __init__(self,image,x,y):
         super().__init__()
         self.image = image
         self.rect = self.image.get_rect() 
+        self.rect.center = (x,y) 
+
+class monster(p.sprite.Sprite):
+    def __init__(self,image,x,y):
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect() 
         self.rect.center = (x,y)   
 
-me = char(monster)
+me = qbert(bert[3])
+m1 = monster(monsters[0],214,586)
+m2 = monster(monsters[2],790,586)
 bert_Group.add(me)
-x = 500
-y = 200
+monster_Group.add(m1,m2)
 n = 0
+
 for i in range(10):
   dx=-(32*i)
   dy=48*i
-  tx=dx+x
+  x=dx+500
   y=200+dy
   for j in range(i+1):
-    b[n] = block(cube[0],tx,y)
-    tx+=64
+    b[n] = block(cube[0],x,y)
+    x+=64
     block_Group.add(b[n])
     n+=1
-print(n)
    
 p.display.set_caption("Bab bert")
 run = True
@@ -61,17 +71,22 @@ while run:
   for event  in p.event.get():
     if event.type == p.QUIT:
       run = False
-  if p.sprite.collide_mask(b[0],me):
-    b[0].image = cube[2]
+  for ty in range(55):     
+    if p.sprite.collide_mask(b[ty],me):
+      b[ty].image = cube[1]
   screen.fill("BLUE")
   mx,my = p.mouse.get_pos()
   print(mx,my)
   me.rect.center = (mx,my)
-  block_Group.clear(screen,screen)
   block_Group.draw(screen)
   bert_Group.draw(screen)
+  monster_Group.draw(screen)
   block_Group.update()
   bert_Group.update()
+  monster_Group.update()
   p.display.update()
+  c = clock.tick(60)
+  tc = c+10
+  me.rot()
 
 
