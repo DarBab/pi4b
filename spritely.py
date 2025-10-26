104d935cc11bb0df2b9912ec70b538a63c3bce87
DarBab <119262402+DarBab@users.noreply.github.com>
2025-10-26 02:04:17 -0400

    Add files via upload

--- spritely.py
+++ spritely.py
@@ -17,20 +17,12 @@ player = [0] * 8
 banner = [0] * 8
 n = 0
 bn = 0
+global pl,tt,tb
 pl = 0
 run = True
 bert_Group = p.sprite.Group()
 block_Group = p.sprite.Group()
 monster_Group = p.sprite.Group()
-banner_Group = p.sprite.Group()
-
-for i in range(8):#bert
-  bert[i] = p.transform.scale2x(sheet.subsurface(i*16,0,16,16))
-for j in range(8):#monsters
-  monsters[j] = p.transform.scale2x(sheet.subsurface(j*16,16,16,16))
-for i in range(3):#blocks
-  cube[i]= sheet.subsurface(0,160+(i*32),32,32)
-  cube[i]= p.transform.scale2x(cube[i])
 
 class qbert(p.sprite.Sprite):
     def __init__(self,image,x,y):
@@ -38,6 +30,7 @@ class qbert(p.sprite.Sprite):
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.bottomleft = (x,y)
+        pl = 0
     def bert_Move(self,x,y,char):
         self.rect.x+=x
         self.rect.y+=y
@@ -59,20 +52,20 @@ class qbert(p.sprite.Sprite):
         self.rect.y+=y
         col = p.sprite.spritecollide(self,tg,0,p.sprite.collide_circle_ratio(.5))
         if col:
-            col[0].image = cube[0]
+            pass
         else:
           self.rect.x = tx
           self.rect.y = ty
-        
-me = qbert(bert[1],518,155)
-m1 = qbert(monsters[1],228,590)
-m2 = qbert(monsters[5],810,590)
-bert_Group.add(me)
-monster_Group.add(m1,m2)
 
+for i in range(8):#bert
+  bert[i] = p.transform.scale2x(sheet.subsurface(i*16,0,16,16))
+for j in range(8):#monsters
+  monsters[j] = p.transform.scale2x(sheet.subsurface(j*16,16,16,16))
+for i in range(3):#blocks
+  cube[i]= sheet.subsurface(0,160+(i*32),32,32)
+  cube[i]= p.transform.scale2x(cube[i])
 for j in range(8):#player banner
   player[j] = p.transform.scale2x(sheet.subsurface(128,j*8+112,52,8))
-  banner[j] = qbert(player[j],32,32)
 for i in range(10):#set up blocks
   x=-(32*i)+500
   y=200+48*i
@@ -81,44 +74,53 @@ for i in range(10):#set up blocks
     x+=64
     block_Group.add(b[n])
     n+=1
+     
+me = qbert(bert[1],518,155)
+m1 = qbert(monsters[1],228,590)
+m2 = qbert(monsters[5],810,590)
+bert_Group.add(me)
+monster_Group.add(m1,m2)
+p.display.set_caption("baburt")
 
 def tunage():
   p.mixer.music.load("/home/pi/Downloads/jump.mp3")
   p.mixer.music.play()
 def render():
   screen.fill("black")
-  banner_Group.update()
   block_Group.update()
   bert_Group.update()
   monster_Group.update()
-  banner_Group.draw(screen)
   block_Group.draw(screen)
   bert_Group.draw(screen)
   monster_Group.draw(screen)
-  p.draw.rect(screen,0,m1.rect,1,2)
+  screen.blit(player[pl],(32,32))            #                         this too
   p.display.update()
 def jump(char,tg):
-    while tt - p.time.get_ticks() >= 0:
+    if tt - p.time.get_ticks() > 0:
       pass
     tx = r.randint(0,1)
     ty = r.randint(0,1)
     char.mon_Move(monster_x[tx],monster_y[ty],tg,char.image)
     render()
-def scre():
+def scre():#                             this need help!!!
   score = 0
   for i in range(50):
     if b[i].image == cube[1]:
       score += 1
     print(score)
-  while yy-p.time.get_ticks() >= 0:
-    pass
-  print('now')
+def banner_scroll():
+    if tb - p.time.get_ticks() > 0:
+      pass
+    global pl
+    pl+=1 
+    if pl > 5:
+      pl = 0         #                         this too
 
-p.display.set_caption("baburt")
 while run == True:
   clock.tick(60)
   mx,my = p.mouse.get_pos()
-#  print(mx,my)
+  print(mx,my)
+  
   for event in p.event.get():
     if event.type == p.KEYDOWN:
       if event.key == p.K_c:
@@ -139,4 +141,6 @@ while run == True:
   tt = p.time.get_ticks()+100
   jump(m1,block_Group)
   jump(m2,block_Group)
+  tb = p.time.get_ticks()+50000
+  banner_scroll()
   render()
